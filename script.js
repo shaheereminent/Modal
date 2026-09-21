f7b7746491fa52083147f5cd14f59b5b1ac70d0c
shaheereminent <shaheer_eminent@hotmail.com>
2026-09-21 22:16:09 +0500

    initial: done some work already

---
+++ script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const DOM = 
+{
+    modal:         document.querySelector    ('.modal'),
+    overlay:       document.querySelector    ('.overlay'),
+    btnCloseModal: document.querySelector    ('.close-modal'),
+    btnOpenModal:  document.querySelectorAll ('.show-modal')
+};
+
+
+// open modal function
+const openModal = function(button)
+{
+    console.log(`button clicked ${button.textContent}`);
+    DOM.modal.classList.remove  ('hidden');
+    DOM.overlay.classList.remove('hidden');
+};
+
+
+// close modal function
+const closeModal = function()
+{
+    DOM.modal.classList.add('hidden');
+    DOM.overlay.classList.add('hidden');
+};
+
+
+// open modal event handler
+for (let i=0; i<DOM.btnOpenModal.length; i++)
+
+    {
+        DOM.btnOpenModal[i].addEventListener('click', () =>  openModal(DOM.btnOpenModal[i]));
+    };
+
+
+// close modal event handler
+DOM.btnCloseModal.addEventListener('click', closeModal);
+DOM.overlay.addEventListener      ('click', closeModal);
